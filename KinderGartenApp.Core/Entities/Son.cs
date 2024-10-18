c07186cbcb2cf0fefbd50d088e06352f824ffec5
Drafj <juanpabloc2000@gmail.com>
2024-10-18 17:31:05 -0500

    data annotations aplied

--- KinderGartenApp.Core/Entities/Son.cs
+++ KinderGartenApp.Core/Entities/Son.cs
@@ -1,12 +1,29 @@
-﻿using System;
+﻿using System.ComponentModel.DataAnnotations;
+
 
 namespace KinderGartenApp.Core.Entities
 {
 	public class Son
 	{
+        [Key]
         public int Id { get; set; }
-        public string Name { get; set; }
-        public Date BirthDate { get; set; }
+        [Required(ErrorMessage = "El nombre es obligatorio.")]
+        [StringLength(100, ErrorMessage = "El nombre no puede tener más de 100 caracteres.")]
+        public string Name { get; set; } = "";
+        [Required]
+        [DataType(DataType.Date)]
+        [CustomValidation(typeof(Son), "ValidarFechaNacimiento")]
+        public DateTime BirthDate { get; set; }
+        [Required(ErrorMessage = "El ID del padre es obligatorio")]
         public int FatherId { get; set; }
+
+        public static ValidationResult ValidarFechaNacimiento(DateTime BirthDate, ValidationContext context)
+        {
+            if (BirthDate > DateTime.Now)
+            {
+                return new ValidationResult("La fecha de nacimiento no puede ser mayor a la fecha actual.");
+            }
+            return ValidationResult.Success;
+        }
     }
 }
