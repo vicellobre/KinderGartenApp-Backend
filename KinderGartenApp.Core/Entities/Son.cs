092e0d8d6d89678e38ebe2ef13bac22b14641cd8
Drafj <juanpabloc2000@gmail.com>
2024-10-18 14:38:38 -0500

    Son entity creation

---
+++ KinderGartenApp.Core/Entities/Son.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KinderGartenApp.Core.Entities
+{
+	public class Son
+	{
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Date BirthDate { get; set; }
+        public int FatherId { get; set; }
+    }
+}
