8f0c56cd7512c644247e52aadd2ca8922441d9d2
Drafj <juanpabloc2000@gmail.com>
2024-10-19 17:07:55 -0500

    son unit tests

--- KinderGartenApp.Core/Entities/Son.cs
+++ KinderGartenApp.Core/Entities/Son.cs
@@ -12,19 +12,31 @@ namespace KinderGartenApp.Core.Entities
         public string Name { get; set; } = "";
         [Required]
         [DataType(DataType.Date)]
-        [CustomValidation(typeof(Son), "ValidarFechaNacimiento")]
+        [CustomValidation(typeof(Son), "ValidateDateBirth")]
         public DateTime BirthDate { get; set; }
         [Required]/////     ya se que no es asi pero lo dejo asi de momento mientras investigo como hacer que se asigne automaticamente, tenganme paciencia estoy aprendiendo
-        public int FatherId { get; set; }
-        public Padre Father { get; set; } = new();
+        public int ParentId { get; set; }
+        public Padre Parent { get; set; } = new();
 
-        public static ValidationResult ValidarFechaNacimiento(DateTime BirthDate, ValidationContext context)
+        public static ValidationResult ValidateDateBirth(DateTime BirthDate, ValidationContext context)
         {
             if (BirthDate > DateTime.Now)
             {
                 return new ValidationResult("La fecha de nacimiento no puede ser mayor a la fecha actual.");
             }
             return ValidationResult.Success;
+        }
+
+        public void Validate()
+        {
+            if (string.IsNullOrWhiteSpace(Name) || Name.Length > 100)
+                throw new ArgumentException("El nombre no puede estar vacío y debe tener un máximo de 100 caracteres.");
+
+            if (BirthDate > DateTime.Now)
+                throw new ArgumentException("La fecha de nacimiento no puede ser futura.");
+
+            if (ParentId <= 0)
+                throw new ArgumentException("El ID del padre no puede estar vacío.");
         }
     }
 }
