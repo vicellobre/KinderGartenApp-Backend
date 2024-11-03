114a9b326a0ba73df43a144cc8aefc0b4c8958c0
ViceLlobre <vicente.llobregat94@gmail.com>
2024-11-02 20:08:53 -0400

    creación de la entidad Teacher y complementos
    - Clase Entity como base para entidades
    - Clase Teacher
    - Se definió el tipo GradeLevel para identificar el grado escolar

---
+++ KinderGartenApp.Core/Entities/Teacher.cs
@@ -0,0 +1,31 @@
+﻿using KinderGartenApp.Core.Enumarations;
+using KinderGartenApp.Core.Primitives;
+
+namespace KinderGartenApp.Core.Entities;
+
+/// <summary>
+/// Clase que representa a un maestro en el sistema.
+/// Hereda de la entidad base e incluye propiedades específicas del maestro.
+/// </summary>
+public class Teacher : Entity
+{
+    /// <summary>
+    /// Nombre del maestro.
+    /// </summary>
+    public string? FirstName { get; set; }
+
+    /// <summary>
+    /// Apellido del maestro.
+    /// </summary>
+    public string? LastName { get; set; }
+
+    /// <summary>
+    /// Nivel educativo o grado asignado al maestro, indicando el grupo de estudiantes a los que enseña.
+    /// </summary>
+    public GradeLevel GradeLevel { get; set; }
+
+    /// <summary>
+    /// Colección de estudiantes (niños) asignados al maestro.
+    /// </summary>
+    //public ICollection<Children> Students { get; set; }
+}
---
+++ KinderGartenApp.Core/Primitives/Entity.cs
@@ -0,0 +1,99 @@
+﻿namespace KinderGartenApp.Core.Primitives;
+
+/// <summary>
+/// Clase base abstracta que representa una entidad con un identificador único de tipo GUID.
+/// Implementa la interfaz IEquatable para comparar entidades.
+/// </summary>
+public abstract class Entity : IEquatable<Entity>
+{
+    /// <summary>
+    /// Identificador único de la entidad.
+    /// Este campo se inicializa al momento de creación y no puede modificarse.
+    /// </summary>
+    public Guid Id { get; private init; }
+
+    // Constructores
+    /// <summary>
+    /// Constructor protegido sin parámetros para inicializar la entidad.
+    /// </summary>
+    protected Entity() { }
+
+    /// <summary>
+    /// Constructor protegido que permite inicializar la entidad con un identificador específico.
+    /// </summary>
+    /// <param name="id">Identificador único de la entidad.</param>
+    protected Entity(Guid id) => Id = id;
+
+    /// <summary>
+    /// Sobrecarga del operador == para comparar dos entidades.
+    /// </summary>
+    /// <param name="first">Primera entidad a comparar.</param>
+    /// <param name="second">Segunda entidad a comparar.</param>
+    /// <returns>Devuelve true si ambas entidades son iguales; de lo contrario, false.</returns>
+    public static bool operator ==(Entity? first, Entity? second)
+    {
+        return first is not null && second is not null && first.Equals(second);
+    }
+
+    /// <summary>
+    /// Sobrecarga del operador != para comparar dos entidades.
+    /// </summary>
+    /// <param name="first">Primera entidad a comparar.</param>
+    /// <param name="second">Segunda entidad a comparar.</param>
+    /// <returns>Devuelve true si las entidades son diferentes; de lo contrario, false.</returns>
+    public static bool operator !=(Entity? first, Entity? second)
+    {
+        return !(first == second);
+    }
+
+    /// <summary>
+    /// Determina si la entidad actual es igual a otra entidad del mismo tipo.
+    /// </summary>
+    /// <param name="other">Otra entidad para comparar con esta instancia.</param>
+    /// <returns>Devuelve true si las entidades son iguales; de lo contrario, false.</returns>
+    public bool Equals(Entity? other)
+    {
+        if (other is null)
+        {
+            return false;
+        }
+
+        if (other.GetType() != GetType())
+        {
+            return false;
+        }
+
+        return other.Id == Id;
+    }
+
+    /// <summary>
+    /// Determina si la entidad actual es igual a otro objeto.
+    /// </summary>
+    /// <param name="obj">Objeto a comparar con esta instancia.</param>
+    /// <returns>Devuelve true si el objeto es una entidad y es igual a esta instancia; de lo contrario, false.</returns>
+    public override bool Equals(object? obj)
+    {
+        if (obj is null)
+        {
+            return false;
+        }
+
+        if (obj.GetType() != GetType())
+        {
+            return false;
+        }
+
+        if (obj is not Entity entity)
+        {
+            return false;
+        }
+
+        return entity.Id == Id;
+    }
+
+    /// <summary>
+    /// Devuelve un código hash para la entidad, basado en el identificador único.
+    /// </summary>
+    /// <returns>Código hash de la entidad.</returns>
+    public override int GetHashCode() => Id.GetHashCode() * 41;
+}
