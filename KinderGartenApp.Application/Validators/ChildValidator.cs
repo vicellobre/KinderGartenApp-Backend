5c5861b1d302dd990313d6d73751804fb8c25cef
Drafj <juanpabloc2000@gmail.com>
2024-11-17 13:26:14 -0500

    Comments added

--- KinderGartenApp.Application/Validators/ChildValidator.cs
+++ KinderGartenApp.Application/Validators/ChildValidator.cs
@@ -6,13 +6,31 @@ using KinderGartenApp.Core.Extensions;
 
 namespace KinderGartenApp.Application.Validators;
 
+/// <summary>
+/// Clase estática que proporciona métodos de validación para la entidad Child.
+/// </summary>
 public static class ChildValidator
 {
+    /// <summary>
+    /// Longitud máxima permitida para nombres.
+    /// </summary>
     private const int MaxNamesLenght = 50;
+
+    /// <summary>
+    /// Edad mínima requerida para un niño.
+    /// </summary>
     private const int MinimumYearsOld = 6;
 
+    /// <summary>
+    /// Valida los datos de un objeto Child.
+    /// </summary>
+    /// <param name="child">El objeto Child que se va a validar.</param>
+    /// <returns>Un resultado que indica si la validación fue exitosa.</returns>
     public static Result<bool> Validate(Child child)
     {
+        /// <summary>
+        /// Lista para almacenar los errores encontrados durante la validación.
+        /// </summary>
         List<Error> errors = [];
 
         if (string.IsNullOrWhiteSpace(child.FirstName))
@@ -44,6 +62,9 @@ public static class ChildValidator
         if (child.BirthDate > DateTime.Today.AddYears(-MinimumYearsOld))
             errors.Add(Error.BirthDate.InvalidAge(MinimumYearsOld));
 
+        /// <summary>
+        /// Si no hay errores devuelve un resultado exitoso, de lo contrario devuelve la lista de errores
+        /// </summary>
         return errors.IsEmpty() ? Result<bool>.Success(true) : Result<bool>.Failure(errors);
     }
 }
