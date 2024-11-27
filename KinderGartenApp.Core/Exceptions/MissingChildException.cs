bec79c2bbacb1e9084a1879dafd9844daa1d9515
Drafj <juanpabloc2000@gmail.com>
2024-11-27 17:23:47 -0500

    child filter created

---
+++ KinderGartenApp.Core/Exceptions/MissingChildException.cs
@@ -0,0 +1,35 @@
+﻿namespace KinderGartenApp.Core.Exceptions;
+
+/// <summary>
+/// Excepción que se lanza cuando el objeto Child no ha sido seteado antes de realizar una operación.
+/// </summary>
+public class MissingChildException : Exception
+{
+    /// <summary>
+    /// Inicializa una nueva instancia de la clase <see cref="MissingChildException"/> con un mensaje predeterminado.
+    /// </summary>
+    public MissingChildException()
+        : base("Child must be set before performing this operation.")
+    {
+    }
+
+    /// <summary>
+    /// Inicializa una nueva instancia de la clase <see cref="MissingChildException"/> con un mensaje de error especificado.
+    /// </summary>
+    /// <param name="message">El mensaje que describe el error.</param>
+    public MissingChildException(string message)
+        : base(message)
+    {
+    }
+
+    /// <summary>
+    /// Inicializa una nueva instancia de la clase <see cref="MissingChildException"/> con un mensaje de error especificado
+    /// y una referencia a la excepción interna que es la causa de esta excepción.
+    /// </summary>
+    /// <param name="message">El mensaje que describe el error.</param>
+    /// <param name="innerException">La excepción que es la causa de la excepción actual.</param>
+    public MissingChildException(string message, Exception innerException)
+        : base(message, innerException)
+    {
+    }
+}
