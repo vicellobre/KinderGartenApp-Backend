f8f3bb827fafa1f111b2751866e8f65b5326cc15
Drafj <juanpabloc2000@gmail.com>
2025-01-10 21:50:06 -0500

    Add parent Tests

---
+++ KinderGartenApp.Tests/Application/DTOs/Children/AddParent/AddParentMessageTests.cs
@@ -0,0 +1,25 @@
+﻿using KinderGartenApp.Application.DTOs.Children.AddParent;
+
+namespace KinderGartenApp.Tests.Application.DTOs.Children.AddParent;
+
+public class AddParentMessageTests
+{
+    [Fact]
+    public void AddParentMessage_ShouldInitializeProperly()
+    {
+        // Arrange
+        var studentId = Guid.NewGuid();
+        var parentId = Guid.NewGuid();
+
+        // Act
+        var message = new AddParentMessage
+        {
+            ChildId = studentId,
+            ParentId = parentId
+        };
+
+        // Assert
+        Assert.Equal(studentId, message.ChildId);
+        Assert.Equal(parentId, message.ParentId);
+    }
+}
---
+++ KinderGartenApp.Tests/Application/DTOs/Children/AddParent/AddParentResponseTests.cs
@@ -0,0 +1,37 @@
+﻿using KinderGartenApp.Application.DTOs.Children.AddParent;
+
+namespace KinderGartenApp.Tests.Application.DTOs.Children.AddParent;
+
+public class AddParentResponseTests
+{
+    [Fact]
+    public void AddParentResponse_ShouldInitializeProperly()
+    {
+        // Arrange
+        var studentId = Guid.NewGuid();
+        var studentFirstName = "David";
+        var studentLastName = "Martinez";
+        var parentId = Guid.NewGuid();
+        var parentFirstName = "Mateo";
+        var parentLastName = "Quiceno";
+
+        // Act
+        var response = new AddParentResponse
+        {
+            ParentId = parentId,
+            ParentFirstName = parentFirstName,
+            ParentLastName = parentLastName,
+            ChildId = studentId,
+            ChildFirstName = studentFirstName,
+            ChildLastName = studentLastName
+        };
+
+        // Assert
+        Assert.Equal(studentId, response.ChildId);
+        Assert.Equal(studentFirstName, response.ChildFirstName);
+        Assert.Equal(studentLastName, response.ChildLastName);
+        Assert.Equal(parentId, response.ParentId);
+        Assert.Equal(parentFirstName, response.ParentFirstName);
+        Assert.Equal(parentLastName, response.ParentLastName);
+    }
+}
